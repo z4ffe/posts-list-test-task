ced7cff5c4126d88dbf395b04c6e0574ab646d58
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-06-14 11:08:41 +0300

    delete user page link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,13 @@
 import {Button, Container, Form, Nav, Navbar, Stack} from 'react-bootstrap'
-import {Link, useLocation} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export const Header = () => {
-	const location = useLocation()
-	const isUserPage = location.pathname.includes('user/')
-
 	return (
 		<Container className='border-bottom'>
 			<Stack className='justify-content-between' direction='horizontal'>
 				<Navbar variant='light'>
 					<Nav>
 						<Nav.Link as={Link} to='/'>Posts</Nav.Link>
-						{isUserPage && <Nav.Link as={Link} to='user/1'>User</Nav.Link>}
 						<Nav.Link as={Link} to='aboutme'>About Me</Nav.Link>
 					</Nav>
 				</Navbar>
